acf8a55da9895acfde4ff27b8524edec7cfcd654
spaenleh <spaenleh@gmail.com>
2025-08-06 13:10:34 +0200

    feat: add auth via generators

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :admin, :scopes,
+  user: [
+    default: true,
+    module: Admin.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: Admin.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :admin,
   ecto_repos: [Admin.Repo],
   generators: [timestamp_type: :utc_datetime]
