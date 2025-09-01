14934aa5af78f184d52f3c9c5677f84dfd948aa8
spaenleh <spaenleh@gmail.com>
2025-09-01 13:16:07 +0200

    fix: tests

--- test/admin_web/live/user_live/form_test.exs
+++ test/admin_web/live/user_live/form_test.exs
@@ -6,10 +6,14 @@ defmodule AdminWeb.UserLive.FormTest do
     setup :register_and_log_in_user
 
     test "renders form for new user", %{conn: conn} do
-      {:ok, view, html} = live(conn, ~p"/users/new")
+      {:ok, lv, html} = live(conn, ~p"/users/new")
 
       assert html =~ "Create a new User"
-      assert has_element?(view, "form")
+      assert has_element?(lv, "form")
+
+      assert form(lv, "#user_form", user: %{email: "test@example.com"})
+             |> render_submit()
+             |> follow_redirect(conn, ~p"/users")
     end
   end
 end
