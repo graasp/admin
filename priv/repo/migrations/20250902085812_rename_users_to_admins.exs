731e020c92e28b629d8b0ece7150148e70d41157
spaenleh <spaenleh@gmail.com>
2025-09-04 16:11:28 +0200

    fix: foreign key

--- priv/repo/migrations/20250902085812_rename_users_to_admins.exs
+++ priv/repo/migrations/20250902085812_rename_users_to_admins.exs
@@ -15,7 +15,7 @@ defmodule YourApp.Repo.Migrations.RenameUsersToAdmins do
     execute "ALTER INDEX users_tokens_user_id_index RENAME TO admins_tokens_user_id_index;"
     execute "ALTER TABLE admins_tokens RENAME CONSTRAINT users_tokens_pkey TO admins_tokens_pkey;"
 
-    execute "ALTER TABLE admins_tokens RENAME CONSTRAINT users_tokens_user_id_users_id_fk TO admins_tokens_user_id_admins_id_fk;"
+    execute "ALTER TABLE admins_tokens RENAME CONSTRAINT users_tokens_user_id_fkey TO admins_tokens_user_id_fkey;"
   end
 
   def down do
