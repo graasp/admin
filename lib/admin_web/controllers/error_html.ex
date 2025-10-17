9bc29dfdc9e57081b688112095feb8813483a1fb
spaenleh <spaenleh@gmail.com>
2025-10-17 12:06:18 +0200

    fix: testing the callback

--- lib/admin_web/controllers/error_html.ex
+++ lib/admin_web/controllers/error_html.ex
@@ -15,16 +15,22 @@ defmodule AdminWeb.ErrorHTML do
   #
   # embed_templates "error_html/*"
 
-  def render("500.html", _assigns) do
+  def render("500.html", assigns) do
     case Sentry.get_last_event_id_and_source() do
       {event_id, :plug} when is_binary(event_id) ->
         opts = Jason.encode!(%{eventId: event_id})
+        assigns = assign(assigns, :opts, opts)
 
-        ~E"""
-        <script src="https://browser.sentry-cdn.com/5.9.1/bundle.min.js" integrity="sha384-/x1aHz0nKRd6zVUazsV6CbQvjJvr6zQL2CHbQZf3yoLkezyEtZUpqUNnOLW9Nt3v" crossorigin="anonymous"></script>
+        ~H"""
+        <script
+          src="https://browser.sentry-cdn.com/5.9.1/bundle.min.js"
+          integrity="sha384-/x1aHz0nKRd6zVUazsV6CbQvjJvr6zQL2CHbQZf3yoLkezyEtZUpqUNnOLW9Nt3v"
+          crossorigin="anonymous"
+        >
+        </script>
         <script>
           Sentry.init({ dsn: '<%= Sentry.get_dsn() %>' });
-          Sentry.showReportDialog(<%= raw opts %>)
+          Sentry.showReportDialog(<%= raw @opts %>)
         </script>
         """
 
