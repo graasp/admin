e68f5e84b3fc9332a5500a1eeb8eb885987f7233
Basile Spaenlehauer <spaenleh@gmail.com>
2025-11-27 11:31:04 +0100

    fix: improve un-publish page (#68)

--- lib/admin_web/live/published_item_live/unpublish.ex
+++ lib/admin_web/live/published_item_live/unpublish.ex
@@ -1,5 +1,6 @@
 defmodule AdminWeb.PublishedItemLive.Unpublish do
   alias Admin.Publications
+  import AdminWeb.PublishedItemHTML
   require Logger
   use AdminWeb, :live_view
 
@@ -19,12 +20,34 @@ defmodule AdminWeb.PublishedItemLive.Unpublish do
           <:item title="Description">
             <.raw_html html={@published_item.item.description} />
           </:item>
+          <:item title="Creator">
+            <.publication_creator publication={@published_item} />
+          </:item>
         </.list>
       </div>
 
       <.form for={@removal_form} id="removal_form" phx-submit="submit" phx-change="validate">
+        <div role="alert" class="alert alert-info alert-soft mb-2">
+          <.icon name="hero-exclamation-circle" />
+          <span>
+            Please provide a reason for the removal. The reason will be communicated to the publication creator by email. Remember to be kind and to respect others.
+          </span>
+        </div>
+
         <.input field={@removal_form[:reason]} type="textarea" label="Reason" required />
-        <.button variant="primary" phx-disable-with="Unpublishing ...">Unpublish</.button>
+
+        <%= if @published_item.creator_id do %>
+          <div role="alert" class="alert alert-warning alert-soft mb-2">
+            <.icon name="hero-exclamation-triangle" />
+            <span>
+              This publication will be removed from the Library and an email will be sent to <strong>{@published_item.creator.email}</strong>.
+            </span>
+          </div>
+        <% end %>
+
+        <.button type="submit" variant="primary" phx-disable-with="Unpublishing ...">
+          Unpublish and notify
+        </.button>
       </.form>
 
       <.button navigate={~p"/published_items/#{@published_item}"}>
