c8aa09ea4db29396efd090160651e5bd8c80432e
spaenleh <spaenleh@gmail.com>
2025-12-02 14:22:13 +0100

    fix: issue with compile time config not matching runtime

--- lib/admin/item_thumbnails.ex
+++ lib/admin/item_thumbnails.ex
@@ -1,7 +1,12 @@
 defmodule Admin.ItemThumbnails do
+  @moduledoc """
+  Module for getting signed urls for item thumbnails.
+  """
+
   alias Admin.S3
 
-  @file_bucket Application.compile_env(:admin, :file_items_bucket, "file_items")
+  defp file_bucket, do: Application.get_env(:admin, :file_items_bucket, "file_items")
+
   def get_item_thumbnails(item_id) do
     %{
       small: get_item_thumbnail(item_id, "small"),
@@ -11,7 +16,7 @@ defmodule Admin.ItemThumbnails do
   end
 
   defp get_item_thumbnail(item_id, size) when size in ["small", "medium", "large", "original"] do
-    url = S3.get_object_url(@file_bucket, "thumbnails/#{item_id}/#{size}")
+    url = S3.get_object_url(file_bucket(), "thumbnails/#{item_id}/#{size}")
     url
   end
 end
