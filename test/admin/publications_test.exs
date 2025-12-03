65e7232a4a8f9296cb84b09fa1f98384a8710729
spaenleh <spaenleh@gmail.com>
2025-12-03 11:19:31 +0100

    fix: test

--- test/admin/publications_test.exs
+++ test/admin/publications_test.exs
@@ -17,10 +17,12 @@ defmodule Admin.PublicationsTest do
 
       published_item =
         published_item_fixture(scope)
-        |> Admin.Publications.with_item()
         |> Admin.Publications.with_creator()
+        |> Admin.Publications.with_item()
 
-      assert Publications.list_published_items() == [published_item]
+      [result] = Publications.list_published_items()
+      assert published_item.creator == result.creator
+      assert published_item.item == result.item
     end
 
     test "get_published_item!/2 returns the published_item with given id" do
