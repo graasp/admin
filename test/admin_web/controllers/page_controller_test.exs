1c51c4812f018aff7732fadb8c1cabd83c4c1998
spaenleh <spaenleh@gmail.com>
2026-01-21 12:04:56 +0100

    fix: update tests

--- test/admin_web/controllers/page_controller_test.exs
+++ test/admin_web/controllers/page_controller_test.exs
@@ -1,11 +1,6 @@
 defmodule AdminWeb.PageControllerTest do
   use AdminWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/admin")
-    assert html_response(conn, 200) =~ "Admin"
-  end
-
   describe "Authenticated routes" do
     setup :register_and_log_in_user
 
