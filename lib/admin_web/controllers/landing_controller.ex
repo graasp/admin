277322a80c90f01e63c556abd766f1cc6de66cd1
spaenleh <spaenleh@gmail.com>
2026-01-29 08:10:50 +0100

    fix: add translations
    fix: credo warnings

--- lib/admin_web/controllers/landing_controller.ex
+++ lib/admin_web/controllers/landing_controller.ex
@@ -1,6 +1,8 @@
 defmodule AdminWeb.LandingController do
   use AdminWeb, :controller
 
+  alias AdminWeb.Plugs.Locale
+
   def index(conn, _params) do
     render(conn, :index)
   end
@@ -11,5 +13,29 @@ defmodule AdminWeb.LandingController do
 
   def contact(conn, _params) do
     render(conn, :contact)
+  end
+
+  def locale(conn, _params) do
+    session_locale = Locale.get_session_locale(conn)
+    http_locale = Locale.get_http_locale(conn)
+
+    render(conn, :locale,
+      session_locale: session_locale,
+      http_locale: http_locale
+    )
+  end
+
+  def change_locale(conn, %{"locale" => locale}) do
+    referrer = conn |> get_req_header("referer") |> List.first()
+
+    conn
+    |> Locale.set_locale(locale)
+    |> redirect(external: referrer)
+  end
+
+  def remove_locale(conn, _params) do
+    conn
+    |> Locale.set_locale(nil)
+    |> redirect(to: ~p"/locale")
   end
 end
