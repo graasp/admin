f1ec73972f17d4a50e1aa4d6eaf944471f3819c9
spaenleh <spaenleh@gmail.com>
2026-02-16 14:47:55 +0100

    fix: update

--- lib/admin_web/controllers/redirection_controller.ex
+++ lib/admin_web/controllers/redirection_controller.ex
@@ -4,8 +4,4 @@ defmodule AdminWeb.RedirectionController do
   def library(conn, _params) do
     redirect(conn, external: Application.get_env(:admin, :library_origin))
   end
-
-  def login(conn, _params) do
-    redirect(conn, external: "#{Application.get_env(:admin, :client_origin)}/auth/login")
-  end
 end
