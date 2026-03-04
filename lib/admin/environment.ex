991ac48f3a415e9e1627a91b6f4d97ebc7749287
spaenleh <spaenleh@gmail.com>
2026-03-04 11:07:36 +0100

    fix: add module doc for credo

--- lib/admin/environment.ex
+++ lib/admin/environment.ex
@@ -1,4 +1,8 @@
 defmodule Admin.Environment do
+  @moduledoc """
+  This module provides environment-related functions for the Admin application.
+  """
+
   def app_url(opts \\ []) do
     path = Keyword.get(opts, :path, "/")
     "https://#{Application.get_env(:admin, :base_host)}#{path}"
--- lib/admin_web/controllers/blog_xml.ex
+++ lib/admin_web/controllers/blog_xml.ex
@@ -1,4 +1,7 @@
 defmodule AdminWeb.BlogXML do
+  @moduledoc """
+  This module defines views in XML related to the blog functionality.
+  """
   use AdminWeb, :xml
 
   embed_templates "blog_xml/*"
