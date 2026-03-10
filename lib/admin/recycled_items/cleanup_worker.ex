3f8fee9890caa2f256317a66156567759da66172
spaenleh <spaenleh@gmail.com>
2026-03-10 16:35:44 +0100

    fix: make fixes for credo

--- lib/admin/recycled_items/cleanup_worker.ex
+++ lib/admin/recycled_items/cleanup_worker.ex
@@ -1,5 +1,10 @@
 defmodule Admin.TrashCleanupWorker do
+  @moduledoc """
+  This worker cleans up the trash by deleting items that have not been restored 3 months.
+  """
   use Oban.Worker, queue: :scheduled
+
+  require Logger
 
   def perform(_args) do
     # fetch the 20k oldest trash items
@@ -9,41 +14,51 @@ defmodule Admin.TrashCleanupWorker do
       # get the descendants of the trash item
       descendants = Admin.Items.get_descendants(trash_item_path)
 
-      # remove the associated s3 files
-      files =
-        descendants
-        |> Enum.filter(&(&1.type == "file"))
+      if Enum.any?(descendants, fn d -> d.type == "etherpad" end) do
+        Logger.warning(
+          "Skipping trash item because it contains an etherpad descendant: #{trash_item_path}"
+        )
+      else
+        process_entry(descendants)
 
-      files_data =
-        files
-        |> Enum.map(fn file ->
-          path = file.extra |> get_in(["file", "path"])
-          %{id: file.id, path: path}
-        end)
-
-      # delete file and thumbnails
-      Admin.ItemFiles.delete(files_data)
-
-      # remove associated h5p files in s3
-      h5p_files =
-        descendants
-        |> Enum.filter(&(&1.type == "h5p"))
-
-      for h5p_item <- h5p_files do
-        Admin.H5PItems.delete(h5p_item)
+        # delete items themselves
+        Admin.Items.delete(trash_item_path)
       end
-
-      # notify etherpad that the pad needs to be deleted
-      #
-      #
-
-      # delete items themselves
-      Admin.Items.delete(trash_item_path)
     end
 
     # send an email with the cleanup report containing
     # - the pie chart of the types of items
     # - the creation date of the olderst item and the creation date of the newest item
     # - the number of creators that got item deleted
+  end
+
+  defp process_entry(descendants) do
+    # remove the associated s3 files
+    files =
+      descendants
+      |> Enum.filter(&(&1.type == "file"))
+
+    files_data =
+      files
+      |> Enum.map(fn file ->
+        path = file.extra |> get_in(["file", "path"])
+        %{id: file.id, path: path}
+      end)
+
+    # delete file and thumbnails
+    Admin.ItemFiles.delete(files_data)
+
+    # remove associated h5p files in s3
+    h5p_files =
+      descendants
+      |> Enum.filter(&(&1.type == "h5p"))
+
+    for h5p_item <- h5p_files do
+      Admin.H5PItems.delete(h5p_item)
+    end
+
+    # notify etherpad that the pad needs to be deleted
+    #
+    #
   end
 end
